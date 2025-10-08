b819498b5e17aba23bbbe8dd8ec7474b8308d242
Juliocer <contatodejuliocesar@gmail.com>
2025-10-07 21:04:05 -0300

    Finalizando 2 componenets Button e Header

---
+++ src/components/Button/styles.js
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+export const ButtonContainer = styled.button`
+    background: #565656;
+    border-radius: 2.5rem;
+    border: none;
+    position: relative;
+
+    color: #FFF;
+    padding: 5px 12px;
+    min-width: 120px;
+    width: 100%;
+    margin-right: .5rem;
+
+    ${({variant})=> variant !== "primary" && css`
+        min-width: 167px;
+        height: 33px;
+
+        background: #e4185d;
+
+        &::after {
+            content: '';
+            position: absolute;
+            border: 1px solid #e4105d;
+            top: -5px;
+            left: -6px;
+            width: calc(100% + 10px);
+            height: calc(100% + 10px);
+            border-radius: 22px;
+        }
+    `}
+`
